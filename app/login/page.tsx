f3e8e1e6abadde17f35242b328d3b66801c9484a
Eshe <ehinchliffe@proton.me>
2024-08-23 20:31:54 +0100

    Added basic login page

---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import {Link} from "@nextui-org/link";
+import {
+  Navbar, 
+  NavbarBrand, 
+  NavbarContent, 
+  NavbarItem, 
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem
+} from "@nextui-org/navbar";
+import { Divider } from "@nextui-org/divider"
+import {Input} from "@nextui-org/input";
+import {Button, ButtonGroup} from "@nextui-org/button";
+import { FormEvent } from 'react'
+
+export default function Register() {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      body: formData,
+    })
+ 
+    // Handle response if necessary
+    const data = await response.json()
+    // ...
+  }
+
+  return (
+    <main>
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Crush Matcher</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Link href="/" color="foreground">Home Page</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/register" color="foreground">Register</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/login">Login</Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+    <Divider className="my-4"/>
+    <form onSubmit={onSubmit} className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <Input type="text" name="username" label="Username"/>
+      <Input type="password" name="password" label="Password"/>
+      <Button type="submit" color="primary">Log In</Button>
+    </form>
+    </main>
+  )
+}
