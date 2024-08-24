14813498e54cf1f8283f1d66197bc3b4ddd73096
Eshe <ehinchliffe@proton.me>
2024-08-24 11:32:04 +0100

    Moved heavy computation to client

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,77 +2,24 @@ import type { NextRequest } from 'next/server'
 import { getRequestContext } from '@cloudflare/next-on-pages'
 import { argon2id, sha256 } from 'hash-wasm'
 import { pki, random } from 'node-forge'
+import { ResponseCookies } from 'next/dist/compiled/@edge-runtime/cookies'
 
 export const runtime = 'edge'
 
 export async function POST(request: NextRequest) {
-  // TODO: this all takes too much CPU time for the server
-  // See what can be safely done on the client side (i.e. don't allow a pass-the-hash vulnerability)
-  const formData = await request.formData()
-  const username = formData.get("username") as string
-  const crush = formData.get("crush") as string
-  const password = formData.get("password") as string
-  const secret = formData.get("secret") as string
-  const userCrush = username! + crush!
-  const crushUser = crush! + username!
-  const passwordSalt = random.getBytesSync(16)
-  const maxNum = (2 ** 64) - 1
-  const prng = random.createInstance()
-  // Use the user's secret as a seed
-  prng.seedFileSync = () => secret!
-  // Generate the RSA private and public keys using the seed
-  const { privateKey, publicKey } = pki.rsa.generateKeyPair({ bits: 4096, prng, workers: 2 })
-  const usernameEncrypted = publicKey.encrypt(username)
-  const hashedPassword = await argon2id({
-    password: password,
-    salt: passwordSalt,
-    parallelism: 1,
-    iterations: 256,
-    memorySize: 512,
-    hashLength: 32,
-    outputType: 'encoded',
-  })
-  var derivedUserCrushSalt = 1;
-  username.split('').forEach(letter => {
-    derivedUserCrushSalt = derivedUserCrushSalt * letter.charCodeAt(0) % maxNum
-  })
-  crush.split('').forEach(letter => {
-    derivedUserCrushSalt = derivedUserCrushSalt * letter.charCodeAt(0) % maxNum
-  })
-  const userCrushSalt = await sha256(String(derivedUserCrushSalt))
-  const hashedUserCrush = await argon2id( {
-    password: userCrush,
-    salt: userCrushSalt,
-    parallelism: 1,
-    iterations: 256,
-    memorySize: 512,
-    hashLength: 32,
-    outputType: 'encoded',
-  })
-  const hashedCrushUser = await argon2id( {
-    password: crushUser,
-    salt: userCrushSalt,
-    parallelism: 1,
-    iterations: 256,
-    memorySize: 512,
-    hashLength: 32,
-    outputType: 'encoded',
-  })
-  // How to hash userCrush and crushUser
-  // Idea 1: Hash like passwords (with salt)
-  //  Problem: in order to search, would need to iterate over every user in the database. Not feasible
-  // Idea 2: No salt
-  //  Problem: if database is leaked, all hashes could be considered broken. Not even worth hashing if using this method.
-  // Idea 3: Encrypt usernames
-  //  Encrypt usernames with a user-stored secret, and use no salt for userCrush or crushUser.
-  //  Problem: most usernames are still common, and so rainbow tables would still be a problem
-  // Final idea: encrypt usernames, and use a derived salt
-  //  The salt could be derived (i.e. by the hash of the length of userCrush, or something similar)
-  //  This is beneficial because the salt can be derived when retrieving the userCrush values from the database, (and therefore allows O(1) retrieval),
-  //  but in the event of a DB breach, no usernames are given away, and therefore no values can be broken!
-
   const DB = getRequestContext().env.DB
-  var result = await DB.prepare("INSERT INTO Users (username, password, user_crush, crush_user) VALUES (?1, ?2, ?3, ?4)").bind(usernameEncrypted, hashedPassword, hashedUserCrush, hashedCrushUser).run()
+  const formData = await request.formData()
+  const usernameSecret = formData.get("usernameSecret") as string
+  const username = formData.get("username") as string
+  const userCrush = formData.get("userCrush") as string
+  const crushUser = formData.get("crushUser") as string
+  const password = formData.get("password") as string
+  const usernameExists = (await DB.prepare("SELECT * FROM Usernames WHERE username = ?1").bind(username).run()).results.length > 0
+  // TODO: actual http error codes
+  if (usernameExists) {
+    return new Response("false")
+  }
+  var result = await DB.prepare("INSERT INTO Users (username, password, user_crush, crush_user) VALUES (?1, ?2, ?3, ?4)").bind(usernameSecret, password, userCrush, crushUser).run()
 
   return new Response(String(result.success))
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -14,15 +14,79 @@ import { Divider } from "@nextui-org/divider"
 import {Input} from "@nextui-org/input";
 import {Button, ButtonGroup} from "@nextui-org/button";
 import { FormEvent } from 'react'
+import { argon2id, sha256 } from 'hash-wasm'
+import { pki, random } from 'node-forge'
 
 export default function Register() {
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
  
-    const formData = new FormData(event.currentTarget)
+    var formData = new FormData(event.currentTarget)
+    const username = formData.get("username") as string
+    const crush = formData.get("crush") as string
+    const password = formData.get("password") as string
+    const secret = formData.get("secret") as string
+    const userCrush = username! + crush!
+    const crushUser = crush! + username!
+    const maxNum = (2 ** 64) - 1
+    const usernameSecretlyHashed = await argon2id({
+      password: username,
+      salt: secret,
+      parallelism: 1,
+      iterations: 256,
+      memorySize: 512,
+      hashLength: 32,
+      outputType: "hex",
+    })
+    // TODO: simply hashing username with sha256 is vulnerable to rainbow tables, think of something better
+    const usernameHashed = await sha256(username)
+    const hashedPassword = await argon2id({
+      password: password,
+      salt: secret,
+      parallelism: 1,
+      iterations: 256,
+      memorySize: 512,
+      hashLength: 32,
+      outputType: 'hex',
+    })
+    var derivedUserCrushSalt = 1;
+    username.split('').forEach(letter => {
+      derivedUserCrushSalt = derivedUserCrushSalt * (letter.charCodeAt(0) + 1) % maxNum
+    })
+    crush.split('').forEach(letter => {
+      derivedUserCrushSalt = derivedUserCrushSalt * (letter.charCodeAt(0) + 1) % maxNum
+    })
+    const userCrushSalt = await sha256(String(derivedUserCrushSalt))
+    const hashedUserCrush = await argon2id( {
+      password: userCrush,
+      salt: userCrushSalt,
+      parallelism: 1,
+      iterations: 256,
+      memorySize: 512,
+      hashLength: 32,
+      outputType: 'hex',
+    })
+    const hashedCrushUser = await argon2id( {
+      password: crushUser,
+      salt: userCrushSalt,
+      parallelism: 1,
+      iterations: 256,
+      memorySize: 512,
+      hashLength: 32,
+      outputType: 'hex',
+    })
+    const toSend = new FormData()
+    toSend.set('usernameSecret', usernameSecretlyHashed)
+    toSend.set('username', usernameHashed)
+    toSend.set('password', hashedPassword)
+    toSend.set('userCrush', hashedUserCrush)
+    toSend.set('crushUser', hashedCrushUser)
+
+
+
     const response = await fetch('/api/register', {
       method: 'POST',
-      body: formData,
+      body: toSend,
     })
  
     // Handle response if necessary
